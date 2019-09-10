fd07f6f28e49dbb1099a58ebfb551eea6d5f0fe6
yzy <macy0122@126.com>
2019-09-10 10:08:19 +0800

    update

---
+++ src/main/java/com/yzy/common/config/MyIncerteptor.java
@@ -0,0 +1,38 @@
+package com.yzy.common.config;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @title:
+ * @description:
+ * @package: com.yzy.common.config
+ * @ClassName: com.yzy.common.config.MyIncerteptor.java
+ * @author: yzy
+ * @date: 2019/9/10 9:29
+ * @param:
+ * @return:
+ * @version: v1.0
+ */
+
+public class MyIncerteptor implements HandlerInterceptor {
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		System.out.println("进入拦截器");
+		System.out.println(request.getRequestURL().toString());
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+	}
+}
--- src/main/java/com/yzy/common/config/MyWebMvcConfigurer.java
+++ src/main/java/com/yzy/common/config/MyWebMvcConfigurer.java
@@ -2,17 +2,32 @@ package com.yzy.common.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-class MyWebMvcConfigurer implements WebMvcConfigurer {
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
 
     @Autowired
     BmsConfig bmsConfig;
 
+    /**
+     * 资源注册
+     * @param registry
+     */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/files/**").addResourceLocations("file:///" + bmsConfig.getUploadPath());
+    }
+
+    /**
+     * 拦截器注册
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new MyIncerteptor()).addPathPatterns("/dtb/**").excludePathPatterns("/test/**","/login/**");
     }
 }
