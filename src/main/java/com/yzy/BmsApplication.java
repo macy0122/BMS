b6c520cff30204bf4a77845a9070f7f4d98fabaf
yzy <macy0122@126.com>
2019-09-11 10:22:19 +0800

    update

--- src/main/java/com/yzy/BmsApplication.java
+++ src/main/java/com/yzy/BmsApplication.java
@@ -2,7 +2,6 @@ package com.yzy;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
@@ -10,23 +9,24 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+
 /**
  * @author yzy
  */
 
-@EnableAutoConfiguration(exclude = {
-        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
-})
-@EnableTransactionManagement
-@MapperScan("com.yzy.*.dao")
-@SpringBootApplication
 @EnableCaching
-@ServletComponentScan   //Servlet、Filter、Listener 可以直接通过 @WebServlet、@WebFilter、@WebListener 注解自动注册，无需其他代码。
 @EnableScheduling
 @EnableAsync
+@EnableTransactionManagement
+@ServletComponentScan   //Servlet、Filter、Listener 可以直接通过 @WebServlet、@WebFilter、@WebListener 注解自动注册，无需其他代码。
+@MapperScan("com.yzy.*.dao")
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class BmsApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(BmsApplication.class, args);
-        System.out.println("启动成功");
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BmsApplication.class, args);
+		System.out.println("启动成功");
+	}
 }
+
+
+
