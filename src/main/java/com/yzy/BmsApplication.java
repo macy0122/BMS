659c9fb0d88fcb062c0b381966c193f62401ef3d
yzy <macy0122@126.com>
2019-09-12 11:49:57 +0800

    update

--- src/main/java/com/yzy/BmsApplication.java
+++ src/main/java/com/yzy/BmsApplication.java
@@ -9,11 +9,11 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @title:
- * @description:  springboot 启动类
- *
+ * @description: springboot 启动类
  * @package: com.yzy.BmsApplication.java
  * @author: yzy
  * @date: 2019-09-11 17:42:12
@@ -23,6 +23,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableCaching
 @EnableScheduling
 @EnableAsync
+@EnableSwagger2
 @EnableSwaggerBootstrapUi
 @EnableTransactionManagement
 @ServletComponentScan   //Servlet、Filter、Listener 可以直接通过 @WebServlet、@WebFilter、@WebListener 注解自动注册，无需其他代码。
