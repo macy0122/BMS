1b0c09577ac5fca500d9b302c25428c6fc8255e5
yzy <macy0122@126.com>
2019-09-17 14:04:29 +0800

    update

--- src/main/java/com/yzy/oa/config/WebSocketConfig.java
+++ src/main/java/com/yzy/oa/config/WebSocketConfig.java
@@ -7,46 +7,41 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 /**
- * 通过EnableWebSocketMessageBroker 开启使用STOMP协议来传输基于代理(message broker)的消息,此时浏览器支持使用@MessageMapping 就像支持@RequestMapping一样。
+ * @title:
+ * @description: 通过EnableWebSocketMessageBroker 开启使用STOMP协议来传输基于代理(message broker)的消息,
+ * 此时浏览器支持使用@MessageMapping 就像支持@RequestMapping一样。
+ * @package: com.yzy.oa.config.WebSocketConfig.java
+ * @author: yzy
+ * @date: 2019-09-17 13:53:29
+ * @version: v1.0
  */
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    //    /**
-//     * endPoint 注册协议节点,并映射指定的URl
-//     *
-//     * @param registry
-//     */
-//    @Override
-//    public void registerStompEndpoints(StompEndpointRegistry registry) {
-//        //注册一个Stomp 协议的endpoint,并指定 SockJS协议。
-//        registry.addEndpoint("/endpointWisely").withSockJS();
-//    }
-//
-//    /**
-//     * 配置消息代理(message broker)
-//     *
-//     * @param registry
-//     */
-//    @Override
-//    public void configureMessageBroker(MessageBrokerRegistry registry) {
-//        //广播式应配置一个/topic 消息代理
-//        registry.enableSimpleBroker("/topic");
-//    }
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) { //endPoint 注册协议节点,并映射指定的URl
+	/**
+	 * @title:
+	 * @description: endPoint 注册协议节点,并映射指定的URl
+	 * @param: [registry]
+	 * @return: void
+	 */
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		//注册一个名字为"endpointChat" 的endpoint,并指定 SockJS协议。   点对点-用
+		registry.addEndpoint("/endpointChat").withSockJS();
+	}
 
-        //注册一个名字为"endpointChat" 的endpoint,并指定 SockJS协议。   点对点-用
-        registry.addEndpoint("/endpointChat").withSockJS();
-    }
-
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {//配置消息代理(message broker)
-        //点对点式增加一个/queue 消息代理
-        registry.enableSimpleBroker("/queue", "/topic");
-
-    }
+	/**
+	 * @title:
+	 * @description: 配置消息代理(message broker)
+	 * @param: [registry]
+	 * @return: void
+	 */
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		//点对点式增加一个/queue 消息代理
+        //广播式应配置一个/topic 消息代理
+		registry.enableSimpleBroker("/queue", "/topic");
+	}
 
 }
