fb6b355a8d7ee8fa0efae385fb237ae60cf07c08
yzy <macy0122@126.com>
2019-09-25 08:16:17 +0800

    update

--- src/main/java/com/yzy/common/config/MyWebMvcConfigurer.java
+++ src/main/java/com/yzy/common/config/MyWebMvcConfigurer.java
@@ -2,6 +2,7 @@ package com.yzy.common.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 /**
  * @title:
  * @description: WebMvcConfigurer自定义配置
- *
  * @package: com.yzy.common.config.MyWebMvcConfigurer.java
  * @param:
  * @return:
@@ -21,32 +21,45 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MyWebMvcConfigurer implements WebMvcConfigurer {
 
-    @Autowired
-    BmsConfig bmsConfig;
+	@Autowired
+	BmsConfig bmsConfig;
 
-    /**
-     * @title:
-     * @description: 资源注册
-     *
-     * @param: [registry]
-     * @return: void
-     * @version: v1.0
-     */
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/files/**").addResourceLocations("file:///" + bmsConfig.getUploadPath());
-    }
+	/**
+	 * @title:
+	 * @description: 资源注册
+	 * @param: [registry]
+	 * @return: void
+	 * @version: v1.0
+	 */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/files/**").addResourceLocations("file:///" + bmsConfig.getUploadPath());
+	}
 
-    /**
-     * @title:
-     * @description: 拦截器注册
-     *
-     * @param: [registry]
-     * @return: void
-     * @version: v1.0
-     */
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new MyIncerteptor()).addPathPatterns("/dtb/**").excludePathPatterns("/test/**","/login/**");
-    }
+	/**
+	 * @title:
+	 * @description: 拦截器注册
+	 * @param: [registry]
+	 * @return: void
+	 * @version: v1.0
+	 */
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new MyIncerteptor()).addPathPatterns("/dtb/**").excludePathPatterns("/test/**", "/login/**");
+	}
+
+	/**
+	 * @title: 配置Cors跨域
+	 * @description: 全局配置解决跨域
+	 * @param: [registry]
+	 * @return: void
+	 */
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		registry.addMapping("/corstest")
+//				.allowedOrigins("http://localhost:8888")
+//				.allowedMethods("GET", "POST", "PUT", "DELATE")
+//				.exposedHeaders("*")
+//				.maxAge(3600L);
+//	}
 }
